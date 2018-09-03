809b00e95d9d9558a7c27124be69cf934db66659
Marc Raftesath (Manpower SA (Pty) Ltd) <Marcraffy@gmail.com>
2018-09-03 14:10:57 +0200

    Add create override to dataappservice

--- aspnet-core/src/CoreSignalRTest.Application/Data/DataAppService.cs
+++ aspnet-core/src/CoreSignalRTest.Application/Data/DataAppService.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Abp.Application.Services;
 
 namespace CoreSignalRTest.Data
 {
     public class DataAppService : AsyncCrudAppService<Data,DataDto>
     {
-        public DataAppService(Abp.Domain.Repositories.IRepository<Data, int> repository) : base(repository)
+        public DataAppService(
+            Abp.Domain.Repositories.IRepository<Data, int> repository
+            )
+            : base(repository)
         {
 
+        }
+
+        public override Task<DataDto> Create(DataDto input)
+        {
+            var output = base.Create(input);
+            
+            return output;
         }
     }
 }
