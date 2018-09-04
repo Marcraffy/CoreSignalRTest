e21dd84130ac12e70220385200b0e29a00338fe3
Julia Tsamago (Manpower SA (Pty) Ltd) <v-jutsam@microsoft.com>
2018-09-04 13:50:28 +0200

    will create the table to show all the data

--- angular/src/app/data/data.component.ts
+++ angular/src/app/data/data.component.ts
@@ -1,22 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { HubConnection, HubConnectionBuilder, LogLevel } from "@aspnet/signalr";
+import { PagedRequestDto, PagedListingComponentBase } from '@shared/paged-listing-component-base';
+import { DataDto, DataServiceProxy, PagedResultDtoOfDataDto } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs/operators';
+import { AppComponentBase } from '@shared/app-component-base';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
-export class DataComponent implements OnInit {
+export class DataComponent extends PagedListingComponentBase<DataDto> implements OnInit {
 
+  // @ViewChild('createDataModal') createDataModal: CreateDataComponent;
+
+  active: boolean = false;
+  data: DataDto[] = [];
   syncHub: HubConnection;
-  constructor() { }
+
+  constructor(
+    injector: Injector,
+    private dataService: DataServiceProxy
+  ) {
+    super(injector);
+  }
 
   ngOnInit() {
     abp.event.on("abp.signalr.connected", () => {
       this.syncHub = abp.signalr.hubs.common;
-      this.syncHub.on("DataDto", () =>{
+      this.syncHub.on("DataDto", () => {
         debugger;
       });
     })
+  }
+
+  protected list(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
+    this.dataService.getAll('', request.skipCount, request.maxResultCount)
+      .pipe(finalize(() => {
+        finishedCallback()
+      }))
+      .subscribe((result: PagedResultDtoOfDataDto) => {
+        this.data = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+
+  protected delete(data: DataDto): void {
+    abp.message.confirm(
+      "Delete data '" + data.id + "'?",
+      (result: boolean) => {
+        if (result) {
+          this.dataService.delete(data.id)
+            .subscribe(() => {
+              abp.notify.info("Deleted Data: " + data.id);
+              this.refresh();
+            });
+        }
+      }
+    );
+  }
+
+  // Show Modals
+  createUser(): void {
+    //this.createDataModal.show();
   }
 }
